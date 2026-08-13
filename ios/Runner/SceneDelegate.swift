f9993335eed422e8286e77a06f66cec7df96e286
Ahmet Eren  Aykut <142061629+Area1007@users.noreply.github.com>
2026-08-13 11:37:53 +0300

    Create Flutter root view programmatically for UIScene startup

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,35 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
+  override func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = scene as? UIWindowScene else {
+      super.scene(
+        scene,
+        willConnectTo: session,
+        options: connectionOptions
+      )
+      return
+    }
 
+    let flutterViewController = FlutterViewController(
+      project: nil,
+      nibName: nil,
+      bundle: nil
+    )
+
+    let appWindow = UIWindow(windowScene: windowScene)
+    appWindow.rootViewController = flutterViewController
+    self.window = appWindow
+    appWindow.makeKeyAndVisible()
+
+    super.scene(
+      scene,
+      willConnectTo: session,
+      options: connectionOptions
+    )
+  }
 }
